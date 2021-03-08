78e188f7273e0e58db37a7c664298accec8497cd
BrianLevin <bml201095@gmail.com>
2021-03-08 12:53:37 -0500

    test case trying to create moes account

--- main.cpp
+++ main.cpp
@@ -15,10 +15,20 @@ int main() {
         larrys_account = std::make_unique<Savings_Account>("Larry", -2000.0); // try to create larrys account with an illegal balance
         std::cout << *larrys_account << std::endl;
     }
-      catch (const IllegalBalanceException &ex)
+      catch (const IllegalBalanceException &ex) // created the catch illegal balence exception and then call out function calling the illegal balance
     {
         std::cerr << ex.what() << std::endl;
     }
+  try {
+        moes_account = std::make_unique<Savings_Account>("Moe", 1000.0);
+        std::cout << *moes_account << std::endl;
+        moes_account->withdraw(500.0);
+        std::cout << *moes_account << std::endl;
+        moes_account->withdraw(1000.0);
+        std::cout << *moes_account << std::endl;
+        
+    }
+
     std::cout << "Program completed successfully" << std::endl;
     return 0;
 }
