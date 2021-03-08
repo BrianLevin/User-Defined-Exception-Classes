a43a123760c1fef1d82c592b6e0b6c5bf51b381b
BrianLevin <bml201095@gmail.com>
2021-03-08 12:50:08 -0500

    crated the catch illegal balence exception and then call out function

--- main.cpp
+++ main.cpp
@@ -12,8 +12,12 @@ int main() {
      std::unique_ptr<Account> moes_account; // create two unique pointers
     std::unique_ptr<Account> larrys_account;
      try {
-        larrys_account = std::make_unique<Savings_Account>("Larry", -2000.0);
+        larrys_account = std::make_unique<Savings_Account>("Larry", -2000.0); // try to create larrys account with an illegal balance
         std::cout << *larrys_account << std::endl;
+    }
+      catch (const IllegalBalanceException &ex)
+    {
+        std::cerr << ex.what() << std::endl;
     }
     std::cout << "Program completed successfully" << std::endl;
     return 0;
