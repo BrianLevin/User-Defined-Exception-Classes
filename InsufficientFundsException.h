3480afe31de3a5b7bc7e5ea6026b7512b3f96325
BrianLevin <bml201095@gmail.com>
2021-03-08 12:33:15 -0500

    created insuffircent funds exception default constructors and destructors

--- InsufficientFundsException.h
+++ InsufficientFundsException.h
@@ -2,7 +2,8 @@
 #define __INSUFFICIENT_FUNDS_EXCEPTION_H__
 
 class InsufficientFundsException : public std::exception { // derived publicly from student exception
-
+   InsufficientFundsException() noexcept = default;
+    ~InsufficientFundsException() = default;
 
 
 }
