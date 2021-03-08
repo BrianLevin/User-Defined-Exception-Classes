513a2a266f51920af1a5164af343e6548d8a87d1
BrianLevin <bml201095@gmail.com>
2021-03-08 12:28:54 -0500

    called what virtual function that points to the correct function and makes sure the correct over written function is called to handle the  return exception

--- IllegalBalanceExceptions.h
+++ IllegalBalanceExceptions.h
@@ -5,7 +5,10 @@ class IllegalBalanceException: public std::exception
 {
 public:
     IllegalBalanceException() noexcept = default; // default constructor and garenteeing no exceptions being thrown
-    ~IllegalBalanceException() = default;
+    ~IllegalBalanceException() = default; // default destructor
+     virtual const char *what() const noexcept { //  virtual function called to makesure sure that the correct overridden function is caleld
+        return "Illegal balance exception";
+    }
 };
 
 #endif // __ILLEGAL_BALANCE_EXCEPTION_H__
