9ab0147a0a4cc531252fe18adbecccce2e32e385
BrianLevin <bml201095@gmail.com>
2021-03-08 12:47:23 -0500

    try to create larrys account with a negative balence

--- main.cpp
+++ main.cpp
@@ -1,5 +1,5 @@
 #include <iostream>
-#include <memory>
+#include <memory> // manages dynamic memory
 #include "Account.h"
 #include "Checking_Account.h"
 #include "Savings_Account.h"
@@ -11,6 +11,10 @@ using namespace std;
 int main() {
      std::unique_ptr<Account> moes_account; // create two unique pointers
     std::unique_ptr<Account> larrys_account;
+     try {
+        larrys_account = std::make_unique<Savings_Account>("Larry", -2000.0);
+        std::cout << *larrys_account << std::endl;
+    }
     std::cout << "Program completed successfully" << std::endl;
     return 0;
 }
